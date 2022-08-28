22e06abf8bc6c552bac05c90e82aefe00d72f793
eomjengsig <travelereom@naver.com>
2022-08-28 16:44:28 +0900

    recap_1

--- momentum/app.js
+++ momentum/app.js
@@ -1,31 +1,23 @@
-function sayHello(nameOfPerson, age){
-    console.log("Hello my name is "+ nameOfPerson +" and I'm " + age);
-}
-
-
-sayHello("eom", 10);
-sayHello("jeng", 20);
-sayHello("sig", 30);
-
-
-function plus(a, b) {
+const calculator = {
+  add: function (a, b) {
     console.log(a + b);
-}
-
-plus(2, 5);
-
-function divide(a, b) {
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
     console.log(a / b);
-}
-
-divide(10, 2);
-
-const player = {
-    name: "nico",
-    sayHi: function(otherPersonName) {
-        console.log("Hi! " + otherPersonName + " nice to meet you!");
-    },
+  },
+  times: function (a, b) {
+    console.log(a * b);
+  },
+  power: function (a, b) {
+    console.log(a ** b);
+  },
 };
 
-console.log(player.name);
-player.sayHi("chong");
+calculator.add(10, 2);
+calculator.minus(10, 2);
+calculator.divide(10, 2);
+calculator.times(10, 2);
+calculator.power(10, 2);
