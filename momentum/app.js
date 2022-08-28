170f3ca5f918ebbe118b5ebb2a97fb8a52251531
eomjengsig <travelereom@naver.com>
2022-08-28 11:29:58 +0900

    array

--- momentum/app.js
+++ momentum/app.js
@@ -1,4 +1,10 @@
-const amIFat = null;
-let something;
-console.log(amIFat);
-console.log(something);
+const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+
+// Get Item from Array
+console.log(dayOfWeek); 
+
+// Add one more day to the array
+dayOfWeek.push("sun");
+
+console.log(dayOfWeek); 
