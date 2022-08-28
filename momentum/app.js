febd7239586bc6640c47627484132bb25b663228
eomjengsig <travelereom@naver.com>
2022-08-28 17:20:48 +0900

    return_1

--- momentum/app.js
+++ momentum/app.js
@@ -1,23 +1,8 @@
-const calculator = {
-  add: function (a, b) {
-    console.log(a + b);
-  },
-  minus: function (a, b) {
-    console.log(a - b);
-  },
-  divide: function (a, b) {
-    console.log(a / b);
-  },
-  times: function (a, b) {
-    console.log(a * b);
-  },
-  power: function (a, b) {
-    console.log(a ** b);
-  },
-};
+const age = 96;
+function calculateKrAge(ageOfForeigner) {
+  return ageOfForeigner + 2;
+}
 
-calculator.add(10, 2);
-calculator.minus(10, 2);
-calculator.divide(10, 2);
-calculator.times(10, 2);
-calculator.power(10, 2);
+const krAge = calculateKrAge(age);
+
+console.log(krAge);
