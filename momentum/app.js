07a760cb4168f90ed2c7c0a6347fdd7b71ddb292
eomjengsig <travelereom@naver.com>
2022-08-28 11:06:21 +0900

    first

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,12 @@
-alert("hi");
+const a = 5;
+let b = 2;
+let myName = "eom";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("Hello " + myName);
+
+myName = "eomjengsig"
+
+console.log("Hello " + myName);
