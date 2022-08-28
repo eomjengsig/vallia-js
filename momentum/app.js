52a551bcc141888a72e193d0e92753f165f2b21d
eomjengsig <travelereom@naver.com>
2022-08-28 11:14:35 +0900

    2nd

--- momentum/app.js
+++ momentum/app.js
@@ -1,12 +1,4 @@
-const a = 5;
-let b = 2;
-let myName = "eom";
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("Hello " + myName);
-
-myName = "eomjengsig"
-
-console.log("Hello " + myName);
+const amIFat = null;
+let something;
+console.log(amIFat);
+console.log(something);
