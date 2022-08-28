8f3b5bbcd50957ea9b17bae959b5546969bf23ff
eomjengsig <travelereom@naver.com>
2022-08-28 12:12:04 +0900

    function

--- momentum/app.js
+++ momentum/app.js
@@ -1,19 +1,31 @@
+function sayHello(nameOfPerson, age){
+    console.log("Hello my name is "+ nameOfPerson +" and I'm " + age);
+}
+
+
+sayHello("eom", 10);
+sayHello("jeng", 20);
+sayHello("sig", 30);
+
+
+function plus(a, b) {
+    console.log(a + b);
+}
+
+plus(2, 5);
+
+function divide(a, b) {
+    console.log(a / b);
+}
+
+divide(10, 2);
+
 const player = {
-    name: "eom",
-    points: 10,
-    fat: true,
+    name: "nico",
+    sayHi: function(otherPersonName) {
+        console.log("Hi! " + otherPersonName + " nice to meet you!");
+    },
 };
 
-
-console.log(player);
-
-player.fat = false;
-
-// 두개는 같음
 console.log(player.name);
-console.log(player["name"]);
-
-player.lastName = "Jeng"
-player.points = player.points + 15;
-
-console.log(player);
+player.sayHi("chong");
