3d2c8123ba0d59eb8cab38f43c3d96e19695d14f
eomjengsig <travelereom@naver.com>
2022-08-30 00:29:37 +0900

    css-js-2

--- momentum/app.js
+++ momentum/app.js
@@ -1,13 +1,11 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue") {
-    newColor = "tomato";
+  const clickedClass = "clicked";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
   } else {
-    newColor = "blue";
+    h1.classList.add(clickedClass);
   }
-  h1.style.color = newColor;
 }
 h1.addEventListener("click", handleTitleClick);
