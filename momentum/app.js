89c11ec3e33051f19a608c3c41e0e46f967512e5
eomjengsig <travelereom@naver.com>
2022-08-29 22:35:35 +0900

    conditional-2

--- momentum/app.js
+++ momentum/app.js
@@ -1,15 +1,5 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelectorAll(".hello h1");
 
-if (isNaN(age) || age < 0) {
-  console.log("Only positive number,please.");
-} else if (age < 18) {
-  console.log("You are too young.");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink.");
-} else if (age === 100) {
-  console.log("WoW!");
-} else if (age > 50) {
-  console.log("Stop drink.");
-}
+console.log(title);
 
-// if((a && b) || (c && d)) {} 이렇게도 가능
+title.innerText = "Got you!";
