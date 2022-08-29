74ee098b6838508cbff464fce903d0bf0aa1a6fb
eomjengsig <travelereom@naver.com>
2022-08-29 23:39:27 +0900

    event-2

--- momentum/app.js
+++ momentum/app.js
@@ -2,7 +2,18 @@ const title = document.querySelector(".hello h1");
 
 function handleTitleClick() {
   console.log("title was clicked!");
+  title.innerText = "Mouse is here!";
   title.style.color = "blue";
 }
 
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
