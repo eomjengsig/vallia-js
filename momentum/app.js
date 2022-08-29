a613411936bd5088fcdaedae046db84f117f2f8f
eomjengsig <travelereom@naver.com>
2022-08-30 00:12:54 +0900

    css-js-1

--- momentum/app.js
+++ momentum/app.js
@@ -1,40 +1,13 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  console.log("title was clicked!");
-  h1.innerText = "Mouse is here!";
-  h1.style.color = "blue";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
-
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("no Wifi!!");
-}
-function handleWindowOnline() {
-  alert("good!");
-}
-
-//둘 다 가능 add가 더 좋음
-h1.onclick = handleTitleClick;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
