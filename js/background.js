c1d166964d6d93fa6b0a10bb21db5d5c0c48efe2
eomjengsig <travelereom@naver.com>
2022-09-04 18:01:18 +0900

    page

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["bg_1.jpeg", "bg_2.jpeg", "bg_3.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
---
+++ js/quotes.js
@@ -0,0 +1,50 @@
+const quotes = [
+  {
+    quote: "명언1",
+    author: "000의 엄",
+  },
+  {
+    quote: "명언2",
+    author: "000의 엄",
+  },
+  {
+    quote: "명언3",
+    author: "000의 엄",
+  },
+  {
+    quote: "명언4",
+    author: "000의 엄",
+  },
+  {
+    quote: "명언5",
+    author: "000의 엄",
+  },
+  {
+    quote: "명언6",
+    author: "000의 엄",
+  },
+  {
+    quote: "명언7",
+    author: "000의 엄",
+  },
+  {
+    quote: "명언8",
+    author: "000의 엄",
+  },
+  {
+    quote: "명언9",
+    author: "000의 엄",
+  },
+  {
+    quote: "명언10",
+    author: "000의 엄",
+  },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
